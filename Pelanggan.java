50645792ceb327daf06174ed93aee25c7135a7ae
Andreas Panangian Tamba <andreastamba04@gmail.com>
2023-11-07 09:50:03 +0800

    Ubah Pelanggan sama App bang

--- Pelanggan.java
+++ Pelanggan.java
@@ -1,5 +1,24 @@
 public class Pelanggan {
-    String nama;
-    String nomor_telepon;
-    String alamat;
+    private String _nama;
+    private String nomor_telepon;
+    private String alamat;
+
+    public void set_pelanggan(String nama, String nomor_telepon, String alamat) {
+        this._nama = nama;
+        this.nomor_telepon = nomor_telepon;
+        this.alamat = alamat;
+    }
+
+    public String getNama() {
+        String nama = "Nama anda adalah: " + this._nama;
+        return nama;
+    }
+
+    public String getNomorTelepon() {
+        return this.nomor_telepon;
+    }
+
+    public String getAlamat() {
+        return this.alamat;
+    }
 }
--- app.java
+++ app.java
@@ -1,5 +1,12 @@
 public class app {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Pelanggan _pelanggan = new Pelanggan();
+
+        _pelanggan.set_pelanggan("Budi", "08123456789", "Jl. Jalan No. 1");
+
+        System.out.println(_pelanggan.getNama());
+        System.out.println("Nomor Telepon: " + _pelanggan.getNomorTelepon());
+        System.out.println("Alamat: " + _pelanggan.getAlamat());
     }
-}
+} 
