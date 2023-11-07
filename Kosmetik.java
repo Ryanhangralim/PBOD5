db93746025bfb45f19dfb85a4fe3919f157b3e10
AndreasPTamba <andreastamba04@gmail.com>
2023-11-07 13:32:34 +0800

    encapsulation kosmetik

--- Kosmetik.java
+++ Kosmetik.java
@@ -1,5 +1,23 @@
 public class Kosmetik extends Produk {
-    String jenis_kosmetik;
-    String tanggal_kadaluwarsa;
-    int berat_bersih;
+    private String jenis_kosmetik;
+    private String tanggal_kadaluwarsa;
+    private int berat_bersih;
+
+    public void set_kosmetik(String jenis_kosmetik, String tanggal_kadaluwarsa, int berat_bersih){
+        this.jenis_kosmetik = jenis_kosmetik;
+        this.tanggal_kadaluwarsa = tanggal_kadaluwarsa;
+        this.berat_bersih = berat_bersih;
+    }
+
+    public String get_jeniskosmetik(){
+        return this.jenis_kosmetik;
+    }
+
+    public String get_tanggalkadaluwarsa(){
+        return this.tanggal_kadaluwarsa;
+    }
+
+    public int get_beratbersih(){
+        return this.berat_bersih;
+    }
 }
